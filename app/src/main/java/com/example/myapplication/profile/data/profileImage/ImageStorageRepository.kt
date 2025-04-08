31f2dc7542d0eb11d3027427314c0fab7290cbc6
Vsevolod Sazonov <legoman056@gmail.com>
2025-04-08 22:37:59 +0700

    продолжаю работу с репозиторием

--- app/src/main/java/com/example/myapplication/profile/data/profileImage/ImageStorageRepository.kt
+++ app/src/main/java/com/example/myapplication/profile/data/profileImage/ImageStorageRepository.kt
@@ -1,8 +1,14 @@
 package com.example.myapplication.profile.data.profileImage
 
+import android.util.Log
+
 class ImageStorageRepository {
     val a = 10
     companion object{
         const val RESULT_CODE = 10
+    }
+
+    private fun loadImage(){
+        Log.d("image", "lalal")
     }
 }
