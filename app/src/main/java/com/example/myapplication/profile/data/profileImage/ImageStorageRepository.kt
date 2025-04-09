096c0433621637ae7ebeff408c009955eba5e2ef
Vsevolod Sazonov <legoman056@gmail.com>
2025-04-09 10:42:14 +0700

    добавление репозитория для работы с файлами

--- app/src/main/java/com/example/myapplication/profile/data/profileImage/ImageStorageRepository.kt
+++ app/src/main/java/com/example/myapplication/profile/data/profileImage/ImageStorageRepository.kt
@@ -3,12 +3,5 @@ package com.example.myapplication.profile.data.profileImage
 import android.util.Log
 
 class ImageStorageRepository {
-    val a = 10
-    companion object{
-        const val RESULT_CODE = 10
-    }
-
-    private fun loadImage(){
-        Log.d("image", "lalal")
-    }
+    
 }
